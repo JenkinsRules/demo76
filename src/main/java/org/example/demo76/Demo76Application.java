3732659d6f7367474c134163cedc947fb676a58f
JenkinsRules <fortnitefrederiklarsen@gmail.com>
2026-02-04 18:32:07 +0100

    klf

---
+++ src/main/java/org/example/demo76/Demo76Application.java
@@ -0,0 +1,13 @@
+package org.example.demo76;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo76Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo76Application.class, args);
+    }
+
+}
