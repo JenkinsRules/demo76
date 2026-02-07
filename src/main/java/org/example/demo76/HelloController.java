8c73f3d2836108f61622b2737b49ad4482d0c447
JenkinsRules <fortnitefrederiklarsen@gmail.com>
2026-02-07 13:30:39 +0100

    klf

--- src/main/java/org/example/demo76/HelloController.java
+++ src/main/java/org/example/demo76/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
         @GetMapping("/hello")
         public String sayHello() {
-            return "Hello Sømand, world! Your Spring Boot controller is working.";
+            return "Hello Frederik!!!, world! Your Spring Boot controller is working.";
         }
 
 }
